88e28c962f9ac8de9eb25288feff6f6d28539678
m-kus <44951260+m-kus@users.noreply.github.com>
2019-07-04 17:07:16 +0300

    set timeout

--- conseil/api.py
+++ conseil/api.py
@@ -10,17 +10,20 @@ class ConseilApi:
     def __init__(self,
                  api_key='bakingbad',
                  api_host='https://conseil-dev.cryptonomic-infra.tech',
-                 api_version=2):
+                 api_version=2,
+                 timeout=15):
         self._api_key = api_key
         self._api_host = api_host
         self._api_version = api_version
+        self._timeout = timeout
 
     def _request(self, method, path, json=None):
         response = requests.request(
             method=method,
             url=f'{self._api_host}/v{self._api_version}/{path}',
             headers={'apiKey': self._api_key},
-            json=json
+            json=json,
+            timeout=self._timeout
         )
         if response.status_code != 200:
             raise ConseilException(f'[{response.status_code}]: {response.text}')
